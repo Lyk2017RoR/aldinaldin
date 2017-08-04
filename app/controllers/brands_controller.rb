7dfa5cb3fe8a06374416e8cca8bbc865ebb4cc70
murtzsarialtun <murtaza.sarialtun@hacettepe.edu.tr>
2017-08-04 16:21:31 +0300

    brand controller bugs are fixed

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,4 +1,18 @@
 class BrandsController < ApplicationController
+
+ before_action :get_brand, only: [:show, :edit, :update, :destroy]
+  def index
+     @brands = Brand.all
+   end
+
+   def new
+     @brand = Brand.new
+
+   end
+
+   def create
+     @brand = Brand.new(brand_params)
+
   if @brand.save
         redirect_to @brand
       else
